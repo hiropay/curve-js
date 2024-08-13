da7e9219110f6f1200a951fc4242a2ac64a6b7ff
lift0xff <bryan.berry@gmail.com>
2024-08-13 12:54:12 +0700

    Add USDM and wUSDM

--- src/constants/coins/xdai.ts
+++ src/constants/coins/xdai.ts
@@ -21,6 +21,10 @@ export const COINS_XDAI: { [index: string]: string } = lowerCaseValues({
 
     // --- EUR ---
     'eure': '0xcB444e90D8198415266c6a2724b7900fb12FC56E',
+
+    usdm: "0x59D9356E565Ab3A36dD77763Fc0d87fEaf85508C", // USDM
+    wudsm: "0x57F5E098CaD7A3D1Eed53991D4d66C45C9AF7812", // wUSDM
+    
 })
 
 export const cTokensXDai = []; //.map((a) => a.toLowerCase());
